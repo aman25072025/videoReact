72cde0442c04b2b8a7b4da934cd092698920bcde
unknown <aman.ansari@aquadsoft.com>
2025-05-08 19:56:24 +0530

    Update backend URL to videonode.onrender.com

--- src/socket.js
+++ src/socket.js
@@ -2,7 +2,7 @@ import io from 'socket.io-client';
 
 // Use deployed server for production, localhost for dev
 // const SERVER_URL ='https://testvideonode.onrender.com'
-const SERVER_URL = 'https://testvideonode.onrender.com';
+const SERVER_URL = 'https://videonode.onrender.com';
 
 const socket = io(SERVER_URL);
 
